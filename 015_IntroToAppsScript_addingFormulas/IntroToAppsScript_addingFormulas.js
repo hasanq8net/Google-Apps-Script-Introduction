cad32cd7c62d130dcbd032ab7826dbd20da15416
Hassan Al Bloushi <56361605+hasanq8net@users.noreply.github.com>
2023-12-06 06:34:51 +0300

    Create IntroToAppsScript_addingFormulas.js

---
+++ 015_IntroToAppsScript_addingFormulas/IntroToAppsScript_addingFormulas.js
@@ -0,0 +1,65 @@
+// add total for each row into our Sheet
+function addRowTotals() {
+  
+  const ss = SpreadsheetApp.getActive();
+  const sheet1 = ss.getSheetByName('Sheet1');
+  const lastRow = sheet1.getLastRow();
+  
+  const data = sheet1.getRange(2,1,lastRow - 1,3).getValues();
+
+  console.log(data); 
+  
+  const totalsArray = [];
+  
+  data.forEach(function(row) {
+    
+    const name = row[0];
+    const cost = row[1];
+    const quantity = row[2];
+    
+    const total = cost * quantity;
+    
+    row.push(total);
+    
+    totalsArray.push([total]);
+    
+    console.log(row);
+    
+  });
+  
+  console.log(totalsArray);
+  
+  // paste data back into Sheet
+  sheet1.getRange(2,4,lastRow - 1,1).setValues(totalsArray);
+
+}
+
+
+// add menu
+// onOpen is a special function
+// runs when your Sheet opens
+function onOpen() {
+  
+  const ui = SpreadsheetApp.getUi();
+  
+  ui.createMenu('Custom Menu')
+    .addItem('Add Row Totals','addRowTotals')
+    .addItem('Add Grand Total','grandTotal')
+    .addToUi();
+  
+}
+
+
+// add grand total function
+function grandTotal() {
+
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet1 = ss.getSheetByName('Sheet1');
+  const lastRow = sheet1.getLastRow();
+  const lastColumn = sheet1.getLastColumn();
+
+  // add total row
+  sheet1.getRange(lastRow + 1,1).setValue('TOTAL');
+  sheet1.getRange(lastRow + 1, lastColumn).setFormula('=SUM(D2:D' + lastRow + ')');
+
+}
